6acad648dfbb3593cc17d3416e5417d1abaa5c4b
Michael J Welsh <michael.welsh.j@gmail.com>
2017-05-29 10:39:43 -0400

    Realistic classification example fixed and now is interactive

--- examples/realistic_classification_example.cpp
+++ examples/realistic_classification_example.cpp
@@ -4,6 +4,9 @@
 #include <yannl.hpp>
 #include "print_matrix.hpp"
 
+#include <iostream>
+#include <string>
+#include <algorithm>
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
@@ -16,8 +19,9 @@ void generate_artificial_training_set(Matrix &data, Matrix &target, size_t set_s
 int main() {
     /*
      * The goal of this network is to take in two numbers and determine if their
-     * sum is positive, zero, or negative. If positive, output is {1, 0, 0}. If
-     * zero, output is {0, 1, 0}. If negative, output is {0, 0, 1}.
+     * sum is positive or negative. Positive is represented by the matrix:
+     * {1, 0}, while negative is represented by the matrix {0, 1}. The two
+     * numbers should be in the domain: [-500, 500].
      */
     Matrix data, target;
     generate_artificial_training_set(data, target, 2000);
@@ -26,11 +30,11 @@ int main() {
     srand(time(NULL));
 
     /*
-     * Create network with 2 input neurons, 3 hidden layers each with 8 neurons
-     * and sigmoid activation function, and 3 output neurons with softmax
+     * Create network with 2 input neurons, 2 hidden layers each with 8 neurons
+     * and tanh activation function, and 2 output neurons with softmax
      * activation function.
      */
-    Network network(CLASSIFICATION, 2, {8, 8, 8}, {sigmoid, sigmoid, sigmoid}, 3, softmax);
+    Network network(CLASSIFICATION, 2, {8, 8}, {tanh, tanh}, 2, softmax);
 
     // Convenience structure for easy parameter filling.
     TrainingParams params;
@@ -39,9 +43,9 @@ int main() {
     params.batch_size = data.rows();
     params.max_iterations = 1000;
     params.epoch_analysis_interval = 10;
-    params.min_accuracy = 0.95;
-    params.learning_rate = 0.12;
-    params.annealing_factor = 250;
+    params.min_accuracy = 0.975;
+    params.learning_rate = 0.1;
+    params.annealing_factor = 50;
     params.regularization_factor = 0.0015;
     params.momentum_factor = 0.95;
     params.shuffle = true;
@@ -51,18 +55,60 @@ int main() {
 
     // Get network's predictions for new data and calculate accuracy.
     data = {
-        {-134.5234, 500.123}, {-234, -60.34}, {-4.5, 4.5}
+        {-134.5234, 500.123},
+        {-234, -60.34},
+        {52.23, 83},
+        {320, -400},
+        {200.1, 500},
+        {-3, 4},
+        {3, -4}
     };
     target = {
-        {1, 0, 0}, {0, 0, 1}, {0, 1, 0}
+        {1, 0},
+        {0, 1},
+        {1, 0},
+        {0, 1},
+        {1, 0},
+        {1, 0},
+        {0, 1}
     };
     network.feed_forward(data);
     print_matrix(network.predict(true));
     printf("Accuracy: %.3f\n", network.accuracy(data, target));
 
-    // Save and load it.
+    // Save and load it (for demonstration purposes).
     network.save("my_network.txt");
     network = Network("my_network.txt");
+
+    // Give user opportunity to enter own data in command line for bleeding edge testing.
+    string input;
+    cout << endl << "Test network on given data? (y\\n): ";
+    cin >> input;
+    cout << endl;
+    transform(input.begin(), input.end(), input.begin(), ::tolower);
+    if (input == "y") {
+        cout << "Enter 2 numbers separated by a space and hit enter to get prediction of sum." << endl
+                << "Numbers should be in the domain [-500, 500]; however, you can use numbers " << endl
+                << "outside this range to test network's extrapolation capabilities." << endl
+                << "Type in \"exit\" to exit." << endl << endl;
+        while (true) {
+            double first, second;
+            cin >> input;
+            transform(input.begin(), input.end(), input.begin(), ::tolower);
+            if (input == "exit") {
+                break;
+            }
+            first = stod(input);
+            cin >> second;
+            data = {{first, second}};
+            network.feed_forward(data);
+            if (network.predict(true) == Matrix{{1, 0}}) {
+                cout << "POSITIVE" << endl << endl;
+            } else {
+                cout << "NEGATIVE" << endl << endl;
+            }
+        }
+    }
 
     return 0;
 }
@@ -72,20 +118,14 @@ void generate_artificial_training_set(Matrix &data, Matrix &target, size_t set_s
     data = Matrix(set_size, 2);
     for (size_t i = 0; i < data.rows(); ++i) {
         double first, second;
-        if (i % 3 == 0) {
-            first = rand() % 1000;
-            second = -first;
-        } else {
-            first = (rand() % 1000) - (rand() % 1000);
-            second = (rand() % 1000) - (rand() % 1000);
-        }
-
+        first = (rand() % 1000) - (rand() % 1000);
+        second = (rand() % 1000) - (rand() % 1000);
         data[i][0] = first;
         data[i][1] = second;
     }
 
     // Generate target next.
-    target = Matrix(set_size, 3);
+    target = Matrix(set_size, 2);
     for (size_t i = 0; i < target.rows(); ++i) {
         double sum = 0;
         for (size_t j = 0; j < data.cols(); ++j) {
@@ -94,10 +134,8 @@ void generate_artificial_training_set(Matrix &data, Matrix &target, size_t set_s
 
         if (sum > 0) {
             target[i][0] = 1;
-        } else if (sum == 0) {
+        } else {
             target[i][1] = 1;
-        } else if (sum < 0) {
-            target[i][2] = 1;
         }
     }
 }
